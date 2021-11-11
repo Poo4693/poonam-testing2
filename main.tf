036ba19ed675a875f030121e3e5115ce48fe453f
Poo4693 <81393034+Poo4693@users.noreply.github.com>
2021-11-10 21:22:29 -0800

    test

--- main.tf
+++ main.tf
@@ -1,14 +1,11 @@
-provider "azurerm" {
-  subscription_id = "729e44c4-c672-4524-ae6f-facbd854d997"
-  client_id       = "3e8d0673-81f6-46a2-9d80-c9a5ec730a75"
-  client_secret   = "4KpdYU40JmZPvLpkrw_-9A46Yc_tQ_18T4"
-  tenant_id       = "5a54b002-1578-40e2-bbc3-9d736951d57f"
-  version         = "2.38.0"
-  features {}
-}
-
 terraform {
     backend "azurerm" {}
+}
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    subscription_id = "729e44c4-c672-4524-ae6f-facbd854d997"
+    skip_provider_registration = "true"
+    features {}
 }
 
 resource "azurerm_key_vault" "example" {
